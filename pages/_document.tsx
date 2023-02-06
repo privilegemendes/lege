177ea7108d44c8fca3659cda4884385668460071
PrivilegeMendes <mendesprivilege@gmail.com>
2023-02-06 17:29:06 +0100

    NextJS + Notion Site migration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,28 +3,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 import { IconContext } from '@react-icons/all-files'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
-        <Html lang='en'>
-          <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon.png'
-            />
-
-            <link rel='manifest' href='/manifest.json' />
-          </Head>
-
-          <body>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-/** Inlined version of noflash.js from use-dark-mode */
+const noFlashDarkMode = `/** Inlined version of noflash.js from use-dark-mode */
 ;(function () {
   var storageKey = 'darkMode'
   var classNameDark = 'dark-mode'
@@ -57,9 +36,28 @@ export default class MyDocument extends Document {
     var isDarkMode = document.body.classList.contains(classNameDark)
     localStorage.setItem(storageKey, JSON.stringify(isDarkMode))
   }
-})();
-`
-              }}
+})(); `
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+        <Html lang='en'>
+          <Head>
+            <link rel='shortcut icon' href='/favicon.ico' />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='32x32'
+              href='/favicon-32x32.png'
+            />
+
+            <link rel='manifest' href='/manifest.json' />
+          </Head>
+
+          <body>
+            <script
+              dangerouslySetInnerHTML={{ __html: noFlashDarkMode, }}
             />
             <Main />
 
--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'legemendes-488679b9f99f4094a8d58928f44980cb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Privilege Portfolio',
+  domain: 'legemendes',
+  author: 'Privilege Mendes',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Privilege Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'privilegemendes',
+  github: 'privilegemendes',
+  linkedin: 'privilegemendes',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -50,16 +50,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
+  //navigationStyle: 'default',
+  navigationStyle: 'custom',
   // navigationLinks: [
   //   {
   //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //     pageId: 'ed4f8075dffe462d8e498e15d33df0b2'
+  //   },
+  //   {
+  //     title: 'Projects',
+  //     pageId: '84735b191b3b4d259ac41b9e347ca5bb'
   //   },
   //   {
   //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //     pageId: '99d705dcf8b64fd6a9cc16e175270c9f'
   //   }
   // ]
 })
