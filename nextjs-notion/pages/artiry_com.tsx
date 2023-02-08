4ba3fb390a4039894ce2f66fede7604f82f22577
PrivilegeMendes <mendesprivilege@gmail.com>
2023-02-08 02:27:37 +0100

    SSG path conflict

--- nextjs-notion/pages/artiry_com.tsx
+++ nextjs-notion/pages/artiry_com.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-const Artiry = () => {
+const Artiry_com = () => {
   const router = useRouter();
 
   useEffect(() => {
@@ -11,4 +11,4 @@ const Artiry = () => {
   return <div>You are being redirected...</div>;
 };
 
-export default Artiry;
+export default Artiry_com;
