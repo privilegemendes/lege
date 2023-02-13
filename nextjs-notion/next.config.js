dc307af1f63b7a53e2b6464ddca931e4aedf8a22
PrivilegeMendes <mendesprivilege@gmail.com>
2023-02-13 02:00:56 +0100

    Mute ESlinting rules during prod build for now

--- nextjs-notion/next.config.js
+++ nextjs-notion/next.config.js
@@ -22,6 +22,11 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+		eslint: {
+			// Warning: This allows production builds to successfully complete even if
+			// your project has ESLint errors.
+					ignoreDuringBuilds: true,
+		}
     extends: [
       'plugin:@next/next/recommended',
       'next',
